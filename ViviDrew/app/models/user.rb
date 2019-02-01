26dc4697503994bf1e23019b27891c02bc6b4ec9
DrewE <andrew.engelstein@gmail.com>
2019-02-01 15:01:38 -0800

    Add User model with session token methods, find_by_credentials, validations, and is_password?

--- ViviDrew/app/models/user.rb
+++ ViviDrew/app/models/user.rb
@@ -9,6 +9,41 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
+require 'bcrypt'
 
 class User < ApplicationRecord
+  after_initialize :ensure_session_token
+
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+  
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+  end
+
+  def self.find_by_credentials(un, pw)
+    user = User.find_by(username: un)
+    return nil unless user
+    user.is_password?(pw) ? user : nil
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
 end
