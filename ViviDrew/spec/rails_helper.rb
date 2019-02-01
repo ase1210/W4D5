48221403426e873e0751695b34daf77ab696be37
DrewE <andrew.engelstein@gmail.com>
2019-02-01 12:20:20 -0800

    Update Gemfile, rails_helper, add validation tests to user_spec

--- ViviDrew/spec/rails_helper.rb
+++ ViviDrew/spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'shoulda/matchers'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -64,14 +65,14 @@ Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     # Choose a test framework:
     with.test_framework :rspec
-    with.test_framework :minitest
-    with.test_framework :minitest_4
-    with.test_framework :test_unit
+    # with.test_framework :minitest
+    # with.test_framework :minitest_4
+    # with.test_framework :test_unit
 
     # Choose one or more libraries:
-    with.library :active_record
-    with.library :active_model
-    with.library :action_controller
+    # with.library :active_record
+    # with.library :active_model
+    # with.library :action_controller
     # Or, choose all of the above:
     with.library :rails
   end
